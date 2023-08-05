696d1448ce287f15d2459dcba3dddfeff837c343
thanawut.b <thanawut.bnk@gmail.com>
2023-08-05 03:03:41 +0000

    Add new file

---
+++ dataviz.r
@@ -0,0 +1,6 @@
+# load ggplot library
+# update this file
+
+library(ggplot2)
+
+ggplot(diamonds, aes(carat, price)) + geom_point()
--- hello_world.r
+++ hello_world.r
@@ -2,3 +2,5 @@
 
 print ("hello world")
 print ("Oppenheimer is good movie")
+
+print ("Consistency is key")
